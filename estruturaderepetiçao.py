55a72bb7186a4f787ef861e87e18d5591b27eb72
Luis Felipe Lira <luisfelipelira@unigranrio.br>
2020-09-28 08:28:42 -0300

    commitando todos os exercícios

---
+++ estruturaderepetiçao.py
@@ -0,0 +1,21 @@
+total = mulher = homem = 0
+condicao = 1
+
+while condicao != 0:
+    sexo = input("Digite um Sexo (M ou F) ou pressione apenas enter para revisar o total: ")
+    sexo = sexo.upper()
+    if sexo in "MF":
+        total += 1
+
+        if sexo == "M":
+            homem += 1
+        elif sexo == "F":
+            mulher += 1
+        else:
+            print('O total de pessoas do sexo feminino  : {}'.format(mulher))
+            print('O total de pessoas do sexo masculino : {}'.format(homem))
+            print('O total de pessoas do inteiro grupo  : {}'.format(total))
+            exit()
+
+    else:
+        print('Opção Inválida')
